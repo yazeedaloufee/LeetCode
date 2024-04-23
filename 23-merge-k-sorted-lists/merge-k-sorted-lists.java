4b70f8d54642906ef5c104a0626e361ec7dcaee8
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-04-23 17:03:53 +0300

    Time: 7 ms (27.84%) | Memory: 44.5 MB (37.66%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,27 +10,48 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        ListNode start = new ListNode();
-        ListNode curr = start;
-        while (true) {
-            int index = findLeastIndex(lists);
-            if(index == -1) break;
-            curr.next = lists[index];
-            lists[index] = lists[index].next;
-            curr = curr.next;
+        if(lists == null || lists.length == 0){
+            return null;
+        }
+        List<ListNode> temp = new ArrayList<>();
+        List<ListNode> temp2 = new ArrayList<>();
 
+        for(ListNode node : lists) {
+            temp.add(node);
         }
-        return start.next;
-    }
-    private int findLeastIndex (ListNode[] list) {
-        int min = Integer.MAX_VALUE;
-        int index = -1;
-        for (int i = 0; i < list.length; i++) {
-            if(list[i] != null && list[i].val < min) {
-                index = i;
-                min = list[i].val;
+
+
+        while(temp.size() > 1) {
+            while(temp.size() > 0) {
+                ListNode node1 = temp.remove(0);
+                ListNode node2 = temp.size() < 1 ? null: temp.remove(0);
+                ListNode result = mergeTwo(node1, node2);
+                temp2.add(result);
             }
+            temp = temp2;
+            temp2 = new ArrayList<ListNode>();
+            
         }
-        return index;
+        return temp.get(0);
+    }
+
+    private ListNode mergeTwo(ListNode node1, ListNode node2){
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while(node1 != null && node2 != null) {
+            if(node1.val < node2.val) {
+                curr.next = node1;
+                node1 = node1.next;
+            } else {
+                curr.next = node2;
+                node2 = node2.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = node2 == null? node1 : node2;
+
+        return dummy.next;
+        
     }
 }
