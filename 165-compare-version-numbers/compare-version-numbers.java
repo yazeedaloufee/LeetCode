8654ff36ca1a351f7b35f3ecd4d14d7149501ca5
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-05-03 18:56:52 +0300

    Time: 0 ms (100.00%) | Memory: 41.4 MB (39.08%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.java
+++ 165-compare-version-numbers/compare-version-numbers.java
@@ -1,38 +1,22 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] v1 = version1.split("\\.");
-        String[] v2 = version2.split("\\.");
-        int i = 0; 
-        while ( i < v1.length && i < v2.length ){
-            int i1 = Integer.valueOf(v1[i]);
-            int i2 = Integer.valueOf(v2[i]);
-
-            if(i1 > i2) {
-                return 1; 
-            }else if (i2 > i1) {
-                return -1;
+        int n = version1.length(), m = version2.length();
+        int i = 0, j = 0;
+        while (i < n || j < m) {
+            int x = 0;
+            for (; i < n && version1.charAt(i) != '.'; ++i) {
+                x = x * 10 + version1.charAt(i) - '0';
             }
-            i++;
+            ++i; 
+            int y = 0;
+            for (; j < m && version2.charAt(j) != '.'; ++j) {
+                y = y * 10 + version2.charAt(j) - '0';
+            }
+            ++j; 
+            if (x != y) {
+                return x > y ? 1 : -1;
+            }
         }
-        if(v1.length == v2.length) {
-            return 0;
-        }else if (v1.length > v2.length) {
-            while(i < v1.length) {
-                if(Integer.valueOf(v1[i]) != 0) {
-                    return 1;
-                }
-                i++;
-            }
-            return 0;
-        }else {
-             while(i < v2.length) {
-                if(Integer.valueOf(v2[i]) != 0) {
-                    return -1;
-                }
-                i++;
-            }
-            return 0;
-        }
-
+        return 0;
     }
 }
