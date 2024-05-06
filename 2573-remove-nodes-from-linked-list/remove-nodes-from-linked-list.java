f92957ce33cca154c0084554f6008598c4f37dad
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-05-06 17:15:43 +0300

    Time: 16 ms (44.20%) | Memory: 74.5 MB (28.82%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -8,35 +8,17 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- // reverse the linked list .. delete and next node that is less than the value. reverse linked list again. 
-
-
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        ListNode reversed = reverse(head);
-        ListNode curr = reversed; 
-        while(curr != null ) {
-            while (curr != null && curr.next != null && curr.val > curr.next.val) {
-                curr.next = curr.next.next;
-            }
-            curr = curr.next;
+        if(head == null) return head; 
+        ListNode rec = removeNodes(head.next);
+
+        if(rec != null && rec.val > head.val) {
+            head  = rec;
+        }else {
+            head.next = rec;
         }
-        return reverse(reversed);
+        return head;
         
     }
-
-    public ListNode reverse(ListNode head) {
-        // dont need to check for null values. 
-        ListNode prev = null; 
-        ListNode curr = head;
-
-        while ( curr != null) {
-            ListNode next = curr.next; 
-            curr.next = prev;
-            prev = curr;
-            curr = next; 
-        }
-        return prev;
-    }
-
 }
