de49be76408eaa7342e8ded8fd3bd399b6f2ce6f
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-05-06 17:58:24 +0300

    Time: 9 ms (72.63%) | Memory: 42 MB (94.62%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,34 +1,22 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int max = 0; 
-        Map<Character, Integer> hm = new HashMap<>();
+        int r=0,l=0;
+        int n=s.length();
+        int hash[]=new int[26];
+         int maxfreq=0;
+         int maxlen=0;
+        while(r<n){
+            hash[s.charAt(r)-'A']++;
+            maxfreq =Math.max(maxfreq,hash[s.charAt(r)-'A']);
 
-        int r = 0;
-        int l = 0; 
-        int maxR = 0;
-        int length = s.length();
-        while (r < length){
-            char c = s.charAt(r); 
-            hm.put(c, hm.getOrDefault(c, 0) +  1);// updating the counter for the current letter
-
-            int rep = hm.get(c);// get the counter 
-            maxR = Math.max(rep, maxR);
-            int width = r - l + 1;// calculate the width 
-            System.out.println(rep);
-            if (maxR + k >= width ){ // check if the width I am it is allowed? and calculat max;
-                max = Math.max(width, max);
-            }else{            // if not start poping charcters from the left side and decrement the counter
-                char cl =s.charAt(l);
-                while( l<r && maxR + k < r - l + 1 ){// keep poping while the width is not allowed.
-                    hm.put(cl, hm.get(cl) -  1);
-                    l++;
-                    cl =s.charAt(l);
-                }
-
+          if((r-l+1)-maxfreq>k){
+                hash[s.charAt(l)-'A']--;
+                l++;
             }
+
+            maxlen=Math.max(maxlen,r-l+1);
             r++;
         }
-        return max;
-        
+        return maxlen;
     }
 }
