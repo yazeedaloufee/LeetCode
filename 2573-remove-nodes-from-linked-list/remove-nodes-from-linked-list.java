46f8ff3f578b1bd401e62f3fc01b767d297b43a6
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-05-06 16:50:23 +0300

    Time: 6 ms (93.91%) | Memory: 68.8 MB (52.49%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ // reverse the linked list .. delete and next node that is less than the value. reverse linked list again. 
+
+
+class Solution {
+    public ListNode removeNodes(ListNode head) {
+        ListNode reversed = reverse(head);
+        ListNode curr = reversed; 
+        while(curr != null && curr.next != null) {
+            while (curr != null && curr.next != null && curr.val > curr.next.val) {
+                curr.next = curr.next.next;
+            }
+            curr = curr.next;
+        }
+        return reverse(reversed);
+        
+    }
+
+    public ListNode reverse(ListNode head) {
+        // dont need to check for null values. 
+        ListNode prev = null; 
+        ListNode curr = head;
+
+        while ( curr != null) {
+            ListNode next = curr.next; 
+            curr.next = prev;
+            prev = curr;
+            curr = next; 
+        }
+        return prev;
+    }
+
+}
