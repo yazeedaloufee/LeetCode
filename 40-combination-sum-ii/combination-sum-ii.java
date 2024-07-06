f6beac9e0814beaba819bfc81c72ebf496825beb
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-07-06 17:45:33 +0300

    Time: 5 ms (23.76%) | Memory: 43.2 MB (44.57%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    int target;
+    int[] cand;
+    List<List<Integer>> output;
+    int[] track;
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        this.target = target;
+        cand = candidates;
+        Arrays.sort(cand);
+        output = new ArrayList<>();
+        track = new int[cand.length];
+        helper(new ArrayList<>(), 0, 0);
+        return output;
+    }
+
+    public void helper(List<Integer> curr, int sum, int index) {
+        if(sum == target) output.add(new ArrayList<>(curr));
+        if(sum > target || index == cand.length) return;
+
+        for (int i = index; i < cand.length; i++) {
+            if(i > index && cand[i] == cand[i - 1])continue;
+                 curr.add(cand[i]);
+                    helper(curr, sum + cand[i],i +1);
+                    curr.remove(curr.size() -1);
+            
+                   
+        }
+    }
+}
