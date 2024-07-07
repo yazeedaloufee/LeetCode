a592fe09f49f4fd7794f3d60e3b425360d33b7b7
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-07-07 16:51:35 +0300

    Time: 107 ms (5.02%) | Memory: 55.4 MB (23.99%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -0,0 +1,65 @@
+class Trie {
+    class Node {
+        Node[] children;
+        boolean isWord;
+        Node() {
+            children = new Node[26];
+            isWord = false;
+        }
+    }
+    Node root;
+
+    public Trie() {
+        root = new Node();
+        
+    }
+    
+    public void insert(String word) {
+        Node curr = root; 
+        for(char c : word.toCharArray()) {
+            int idx = c-'a';
+            if(curr.children[idx] == null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.isWord = true;
+    }
+    
+    public boolean search(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            if(curr.children[word.charAt(i) - 'a'] == null) {
+                return false;
+            }
+                  System.out.println(word.charAt(i));
+            if(i == word.length() - 1 && curr.children[word.charAt(i) - 'a'].isWord == false){
+                return false;
+            }
+
+
+            curr= curr.children[word.charAt(i) - 'a'];
+        }
+        return true;
+        
+    }
+    
+    public boolean startsWith(String prefix) {
+         Node curr = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            if(curr.children[prefix.charAt(i) - 'a'] == null) {
+                return false;
+            }
+            curr= curr.children[prefix.charAt(i) - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
