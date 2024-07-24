5ec1f4f3d85cf41ee611f2bef35f9136370c03cf
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-07-24 18:27:03 +0300

    Time: 15 ms (6.38%) | Memory: 42.6 MB (15.13%) - LeetSync

---
+++ 401-binary-watch/binary-watch.java
@@ -0,0 +1,58 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+
+        Map<Integer, List<String>> hm = new HashMap();
+        Map<Integer, List<String>> mm = new HashMap();
+        // i runs to 59 which the number of possible minutes
+        List<String> output = new ArrayList<>();
+
+        for (int i = 0; i < 60; i++) {
+            // calculate number of 1's for number i;
+            int k = i;
+            int ones = 0;
+            while (k > 0) {
+                ones += k % 2;
+                k = k / 2;
+            }
+
+            List<String> curr = mm.getOrDefault(ones, new ArrayList<>());
+            curr.add(mToString(i));
+            mm.put(ones, curr);
+
+            // i runs to 1 which the number of possible hours
+
+            if (i < 12) {
+                List<String> currh = hm.getOrDefault(ones, new ArrayList<>());
+                currh.add(hToString(i));
+                hm.put(ones, currh);
+            }
+
+        }
+        for (int i = 0, j = 0; i < 4; i++) {
+            for (String s : hm.get(i)) {
+                if (turnedOn - i >= 0 && turnedOn - i < 6) {
+                    for (String m : mm.get(turnedOn - i)) {
+                        System.out.println(turnedOn - i);
+                        System.out.println(i);
+
+                        output.add(s + ":" + m);
+                    }
+                }
+            }
+
+        }
+        return output;
+
+    }
+
+    private String mToString(int m) {
+        if (m < 10) {
+            return "0" + m;
+        }
+        return "" + m;
+    }
+
+    private String hToString(int h) {
+        return "" + h;
+    }
+}
