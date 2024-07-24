28e0b297be042116bb712e8379127743d2bb3b16
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-07-24 18:32:42 +0300

    Time: 8 ms (56.86%) | Memory: 42.5 MB (20.92%) - LeetSync

--- 401-binary-watch/binary-watch.java
+++ 401-binary-watch/binary-watch.java
@@ -1,55 +1,12 @@
 class Solution {
     public List<String> readBinaryWatch(int turnedOn) {
+	var times = new ArrayList<String>();
 
-        Map<Integer, List<String>> hm = new HashMap();
-        Map<Integer, List<String>> mm = new HashMap();
-        // i runs to 59 which the number of possible minutes
-        List<String> output = new ArrayList<>();
-
-        for (int i = 0; i < 60; i++) {
-            // calculate number of 1's for number i;
-            int k = i;
-            int ones = 0;
-            while (k > 0) {
-                ones += k % 2;
-                k = k / 2;
-            }
-
-            List<String> curr = mm.getOrDefault(ones, new ArrayList<>());
-            curr.add(mToString(i));
-            mm.put(ones, curr);
-
-            // i runs to 1 which the number of possible hours
-
-            if (i < 12) {
-                List<String> currh = hm.getOrDefault(ones, new ArrayList<>());
-                currh.add(hToString(i));
-                hm.put(ones, currh);
-            }
-
-        }
-        for (int i = 0, j = 0; i < 4; i++) {
-            for (String s : hm.get(i)) {
-                if (turnedOn - i >= 0 && turnedOn - i < 6) {
-                    for (String m : mm.get(turnedOn - i)) {
-                        output.add(s + ":" + m);
-                    }
-                }
-            }
-
-        }
-        return output;
-
-    }
-
-    private String mToString(int m) {
-        if (m < 10) {
-            return "0" + m;
-        }
-        return "" + m;
-    }
-
-    private String hToString(int h) {
-        return "" + h;
-    }
+	for (var hour = 0; hour < 12; hour++)
+		for (var minute = 0; minute < 60; minute++)
+			if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn)
+				times.add(String.format("%d:%02d", hour, minute));
+				
+	return times;
+}
 }
