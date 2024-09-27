b936e4956abb9e2f5f4707d1c8bb59b9b9c4dca9
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-09-27 22:51:41 +0300

    Time: 2 ms (71.47%) | Memory: 42.2 MB (63.25%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,58 @@
+class Solution {
+    Deque<int[]> q = new ArrayDeque<>();
+    int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+    boolean[][] visited;
+
+    public int orangesRotting(int[][] grid) {
+     visited = new boolean[grid.length][grid[0].length] ;
+        int total = 0;
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 2) {
+                q.add(new int[]{i,j});
+                }   
+                if(grid[i][j] == 2 ||grid[i][j] == 1)total++;
+            }
+        }
+        if(total == 0) return 0;
+
+        int iterations = -1;
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            for(int i = 0; i < size; i++) {
+                int[] current = q.poll();
+                bfs(current, grid);
+            }
+
+            iterations++;
+        }
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) return -1;
+                
+            }
+        }
+        return iterations;
+    }
+
+    private void bfs(int[] curr, int[][] grid) {
+        visited[curr[0]][curr[1]] = true;
+        grid[curr[0]][curr[1]] = 2;
+        for(int i = 0; i < 4; i++) {
+            int newR = curr[0] + directions[i][0];
+            int newC = curr[1] + directions[i][1];
+            if(isValid(newR, newC, grid)){
+
+                visited[newR][newC] = true;
+                q.add(new int[]{newR, newC});
+            }
+        }
+    }
+
+    private boolean isValid(int r, int c,int[][] grid) {
+        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != 1|| visited[r][c]) return false;
+        return true;
+    }
+
+}
