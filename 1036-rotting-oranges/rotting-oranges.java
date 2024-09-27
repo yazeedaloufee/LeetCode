5b44909390e2ae999cc6c98b053c95d79edff25b
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-09-27 23:04:23 +0300

    Time: 2 ms (71.47%) | Memory: 42.1 MB (75.99%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,62 +1,62 @@
 class Solution {
     Deque<int[]> q = new ArrayDeque<>();
-    int[][] directions = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+    int[][] directions = new int[][] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
     boolean[][] visited;
 
     public int orangesRotting(int[][] grid) {
-     visited = new boolean[grid.length][grid[0].length] ;
+        visited = new boolean[grid.length][grid[0].length];
         int total = 0;
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == 2) {
-                q.add(new int[]{i,j});
-                }   
-                if(grid[i][j] == 2 ||grid[i][j] == 1)total++;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new int[] { i, j });
+                }
+                if (grid[i][j] == 2 || grid[i][j] == 1)
+                    total++;
             }
         }
-        if(total == 0) return 0;
+        if (total == 0)
+            return 0;
 
         int iterations = -1;
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int size = q.size();
 
-            for(int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) {
                 int[] current = q.poll();
                 bfs(current, grid);
             }
 
             iterations++;
         }
-                for(int i = 0; i < grid.length; i++) {
-            System.out.println(Arrays.toString(grid[i]));
-        }
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == 1) return -1;
-                
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1)
+                    return -1;
+
             }
         }
-
 
         return iterations;
     }
 
     private void bfs(int[] curr, int[][] grid) {
-        if(visited[curr[0]][curr[1]]) return;
-        for(int i = 0; i < 4; i++) {
+        if (visited[curr[0]][curr[1]])
+            return;
+        for (int i = 0; i < 4; i++) {
             int newR = curr[0] + directions[i][0];
             int newC = curr[1] + directions[i][1];
-            if(isValid(newR, newC, grid)){
-
-            grid[newR][newC] = 2;
-
-                q.add(new int[]{newR, newC});
+            if (isValid(newR, newC, grid)) {
+                grid[newR][newC] = 2;
+                q.add(new int[] { newR, newC });
             }
         }
     }
 
-    private boolean isValid(int r, int c,int[][] grid) {
-        if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != 1|| visited[r][c]) return false;
+    private boolean isValid(int r, int c, int[][] grid) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] != 1 || visited[r][c])
+            return false;
         return true;
     }
 
