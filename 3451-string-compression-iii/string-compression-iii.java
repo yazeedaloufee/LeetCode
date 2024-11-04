91498ba18627d26192fe03dd0781297ae38f7ed8
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-11-04 23:25:10 +0300

    Time: 17 ms (78.32%) | Memory: 45.8 MB (59.19%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.java
+++ 3451-string-compression-iii/string-compression-iii.java
@@ -6,21 +6,13 @@ class Solution {
         for (int i = 0; i < chars.length; i++) {
             int counter = 0;
             char c = chars[i];
-            //// a a a a a a a a a
-
-            ////// 0 1 2 3 4 5 6 7 8
-            // i// 0 1 2 3 4 5 6 7 8
-            // c//
             while (counter + i < chars.length && counter < 9 && chars[counter + i] == c) {
                 counter++;
             }
-
             sb.append(counter);
             sb.append(c);
             i = i + counter -1;
-
         }
         return sb.toString();
-
     }
 }
