9fb8055d5ae02a7dc58e94526c285671dde22131
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2024-11-09 15:59:04 +0300

    Time: 5 ms (33.16%) | Memory: 49.3 MB (62.56%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,50 +1,27 @@
 class Solution {
-    int[][] directions = {{1,0}, {0,1}, {-1, 0}, {0, -1}};
-    char[][] grid;
-    int[][] visited;
-    int count = 0; 
-    
     public int numIslands(char[][] grid) {
-        this.grid = grid;
-        int r = grid.length;
-        int c = grid[0].length;
-        visited = new int[r][c];
-        
-        for(int i = 0; i < r; i++) {
-            for(int j = 0; j < c; j++) {
-                if(grid[i][j] == '1' && visited[i][j] != 1){
-
-                    dfs(i, j);
-                    count++;
+        int counter = 0; 
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (dfs(i, j, grid, visited)) {
+                    counter++;
                 }
             }
         }
-        return count;
+        return counter;
     }
-    
-    public void dfs(int r, int  c) {
-        if(visited[r][c] == 1) {
-            return;
-        }
-        visited[r][c] = 1;
-        
-        for (int[] direction : directions) {
-            int newC = direction[0] + c;
-            int newR = direction[1] + r;
-            
-            if(isValid(newC, newR) ){
-                dfs(newR, newC);
-            }
-        }
-    }
-    
-    private boolean isValid(int x, int y) {
-        
-        if(x < 0 || y < 0 || y >= grid.length || x >= grid[0].length || visited[y][x] == 1 || grid[y][x] == '0' ){
+
+    public boolean dfs(int i, int j, char[][] grid, boolean[][] visited) {
+        if (i < 0 || j < 0 || j >= grid[0].length || i >= grid.length || grid[i][j] == '0' || visited[i][j]) {
             return false;
         }
+        visited[i][j] = true;
+
+        dfs(i  , j - 1 , grid, visited);
+        dfs(i , j + 1 , grid, visited);
+        dfs(i - 1 , j , grid, visited);
+        dfs(i + 1 , j  , grid, visited);
         return true;
     }
-    
-    
 }
