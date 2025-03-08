00e93b42631639eb8f1551c4ff86626608448788
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2025-03-08 13:58:55 +0300

    Time: 6 ms (67.59%) | Memory: 45.3 MB (91.93%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,35 +1,27 @@
-import java.util.*;
-
 class Solution {
     Map<String, PriorityQueue<String>> hm;
     Deque<String> stack;
-
     public List<String> findItinerary(List<List<String>> tickets) {
         hm = new HashMap<>();
         stack = new ArrayDeque<>();
-
-        // Build the adjacency list
-        for (List<String> ticket : tickets) {
-            hm.computeIfAbsent(ticket.get(0), k -> new PriorityQueue<>()).add(ticket.get(1));
+        // create adjacency list
+        for(List<String> ticket : tickets) {
+            hm.computeIfAbsent(ticket.get(0),(x) -> new PriorityQueue<>()).add(ticket.get(1));
         }
 
-        // Start DFS from "JFK"
         dfs("JFK");
-
-        // Convert the stack to a list
         List<String> output = new ArrayList<>();
-        while (!stack.isEmpty()) {
+        while(!stack.isEmpty()) {
             output.add(stack.pollFirst());
         }
-
         return output;
     }
 
-    private void dfs(String current) {
-        PriorityQueue<String> destinations = hm.get(current);
-        while (destinations != null && !destinations.isEmpty()) {
-            dfs(destinations.poll());
+    private void dfs(String dep) {
+        PriorityQueue<String> destenations = hm.get(dep);
+        while(destenations != null && !destenations.isEmpty() ){
+            dfs(destenations.poll());
         }
-        stack.push(current);
+        stack.push(dep);
     }
 }
