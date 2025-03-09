707cca6f122c4f365bb4f807b330196030a1edc3
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2025-03-09 07:28:54 +0300

    Time: 17 ms (27.31%) | Memory: 48.4 MB (25.61%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,45 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> hm = new HashMap<>();
-        
         for(String s : strs) {
-            // build the
-            String key = anagramString(getFrequencyArray(s));
-            List<String> temp = hm.getOrDefault(key, new ArrayList<String>());
-            temp.add(s);
-            hm.put(key, temp);
-            
+            int[] map = new int[26];
+            for (char c : s.toCharArray()) {
+                map[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                sb.append((char)(i +'a'));
+                sb.append(map[i]);
+            }
+            String key = sb.toString();
+            hm.computeIfAbsent(key, (x) -> new ArrayList<>()).add(s);
         }
         List<List<String>> output = new ArrayList<>();
-        for(List<String> curr : hm.values()) {
-            output.add(curr);
+
+        for (List<String> list : hm.values()) {
+            output.add(list);
         }
         return output;
         
-    }
-    
-    
-    
-    
-    
-    private String anagramString(int[] array) {
-        StringBuilder sb = new StringBuilder(); 
-        
-        for(int i = 0; i < array.length; i++) {
-            sb.append("" + array[i] );
-            sb.append('a'+i);
-        }
-        return sb.toString();
-    }
-    
-    private int[] getFrequencyArray(String s) {
-        
-        int[] output = new int[26]; 
-        
-        char[] letters = s.toCharArray();
-        for(char c : letters) {
-            output[c - 'a']++;
-        }
-        return output;
     }
 }
