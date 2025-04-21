3922acecbc4614fa530864c3a0d3815a96e81fa0
Yazeed Alfouee <85050837+yazeedaloufee@users.noreply.github.com>
2025-04-21 06:34:27 +0300

    Time: 14 ms (12.70%) | Memory: 60.4 MB (90.48%) - LeetSync

---
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numberOfArrays(int[] differences, int lower, int upper) {
+        int min = 0, max = 0;
+        int start = 0; 
+        for(int i : differences) {
+            start = (start + i)%1000000007;
+            min = Math.min(min, start );
+            max = Math.max(max, start );
+        }
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+        int range = max - min;
+        max = range + lower;
+        min  = lower;
+        // min = min + lower;
+        // max = max + lower;
+
+        if(upper < max) return 0; 
+
+        return upper - max + 1;
+    }
+}
