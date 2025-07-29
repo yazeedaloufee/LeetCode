cd7cf57975c39076bebb3033c7b11008e08d65a4
Yazeed aloufee <85050837+yazeedaloufee@users.noreply.github.com>
2025-07-29 14:18:33 +0300

    Time: 2 ms (92.22%) | Memory: 41.4 MB (56.90%) - LeetSync

---
+++ 38-count-and-say/count-and-say.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String countAndSay(int n) {
+        if (n == 1) return "1";
+        return encoding(countAndSay(n-1));
+    }
+
+    private String encoding (String input) {
+        if(input == null || input.length() == 0) return "";
+        char[] chars= input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < chars.length;i++) {
+            char c = chars[i];
+            int count = 1;
+            while(i+1 < chars.length && chars[i+1] == c) {
+                i++;
+                count++;
+            }
+            sb.append(count);
+            sb.append(c);
+
+        }
+        return sb.toString();
+
+    }
+}
+
+// base case if n -- 1 return "1";
+// find countAndSay(n - 1)
+// run encoding  algorthythem
+//return it. 
